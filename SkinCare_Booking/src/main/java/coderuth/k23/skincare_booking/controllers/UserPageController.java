a22af2de6f857d187185db7eba45872df909eeca
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-03-16 19:03:15 +0700

    CÃustom Master Page And CÃut Layout ( ÆWith Thymeleaflean Code) P -áºart 1

--- SkinCare_Booking/src/main/java/coderuth/k23/skincare_booking/controllers/UserPageController.java
+++ SkinCare_Booking/src/main/java/coderuth/k23/skincare_booking/controllers/UserPageController.java
@@ -2,12 +2,44 @@ package coderuth.k23.skincare_booking.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserPageController {
-    @GetMapping("/user")
-    public String userPage() {
+    @ModelAttribute("currentURI")
+    public String currentURI(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
+    @GetMapping("/")
+    public String userHomePage() {
         return "user/index"; // Load file src/main/resources/templates/user/index.html
+    }
+
+    @GetMapping("/about-us")
+    public String userAboutUsPage() {
+        return "user/about-us";
+    }
+
+    @GetMapping("/services")
+    public String userServicesPage() {
+        return "user/services";
+    }
+
+    @GetMapping("/contact")
+    public String userContactPage() {
+        return "user/contact";
+    }
+
+    @GetMapping("/master")
+    public String userMasterPage() {
+        return "user/master/masterpage";
+    }
+
+    @GetMapping("/rooms")
+    public String userElementPage() {
+        return "user/rooms";
     }
 }
 
