59113473e9ad46a7162c23f4402c8c530c8c453a
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-04-14 10:26:15 +0700

    Resolve conflict in ManagerPageController when updating staff & therapist list

--- SkinCare_Booking/src/main/java/coderuth/k23/skincare_booking/services/StaffService.java
+++ SkinCare_Booking/src/main/java/coderuth/k23/skincare_booking/services/StaffService.java
@@ -1,11 +1,13 @@
 package coderuth.k23.skincare_booking.services;
 
+import coderuth.k23.skincare_booking.models.SkinTherapist;
 import coderuth.k23.skincare_booking.models.Staff;
 import coderuth.k23.skincare_booking.repositories.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class StaffService {
@@ -17,4 +19,17 @@ public class StaffService {
         return staffRepository.findAll();
     }
 
+    public void deleteStaff(UUID id) {
+        staffRepository.deleteById(id);
+    }
+
+    public Staff updateStaff(UUID id, Staff updatedStaff) {
+        Staff staff = staffRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Staff not found!"));
+        staff.setFullName(updatedStaff.getFullName());
+        staff.setImg(updatedStaff.getImg());
+        staff.setPhone(updatedStaff.getPhone());
+        staff.setLocation(updatedStaff.getLocation());
+        return staffRepository.save(staff);
+    }
 }
